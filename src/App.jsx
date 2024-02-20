c9b83be350cc3ccc9eb3399d8dc30faf00ff8781
SebasstianAhr <sebasstianahr117@gmail.com>
2024-02-20 18:19:21 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./index.css";
+import Table from "./components/Table";
+
+const App = () => {
+  return (
+    <div className="container-body">
+      <h1 className="tittle-main">TABLE</h1>
+      <Table />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Table.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Table = () => {
+  return (
+    <div className="container-table">
+      <table>
+        <thead>
+          <tr>
+            <th>Nombre </th>
+            <th>Descripcion</th>
+            <th>Acciones</th>
+          </tr>
+        </thead>
+      </table>
+    </div>
+  );
+};
+
+export default Table;
